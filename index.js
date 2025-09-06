0eeaeaad45d7691e8c5608719c44a7dc37c21852
Preet Kaler <preetkalerop@gmail.com>
2025-09-06 14:49:33 +0530

    Updated backend URL and fixed API calls

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 // app.use(fileUpload());
 app.use(
     cors({
-        origin:"http://localhost:5173",
+        origin:"https://upcodefrontend1.vercel.app",
         credentials:true,
     })
 )
